d4fda28d023ef0a982bc72cf60d1c2aa367b4522
Theelx <43764914+Theelx@users.noreply.github.com>
2022-04-21 09:51:00 -0400

    Python3 compatibility + formatting

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@
 from setuptools import setup
 
 setup(
-    name = "wifijammer",
-    version = "0.1",
-    author = "Dan McInerney",
-    description = "Continuously jam all wifi clients and access points within range.",
-    keywords = "WiFi 802.11 jammer deauth",
-    url = "https://github.com/DanMcInerney/wifijammer",
-    scripts=['wifijammer'],
+    name="wifijammer",
+    version="0.1",
+    author="Dan McInerney",
+    description="Continuously jam all wifi clients and access points within range.",
+    keywords="WiFi 802.11 jammer deauth",
+    url="https://github.com/DanMcInerney/wifijammer",
+    scripts=["wifijammer"],
     # py_modules=['wifijammer'],
-    install_requires=['scapy'],
+    install_requires=["scapy"],
     long_description="Continuously jam all wifi clients and access points within range.",
     classifiers=[
         "Development Status :: 3 - Alpha",
